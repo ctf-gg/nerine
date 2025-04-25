730493a545e8e497e3c333c69c78d043d6c5de93
anirudhb <anirudhb@users.noreply.github.com>
2025-04-25 14:38:17 -0700

    Make initial migration reversible

---
+++ migrations/20250425160517_setup.down.sql
@@ -0,0 +1,14 @@
+DROP INDEX
+    teams_public_id_index,
+    challenges_public_id_index,
+    submissions_by_own_team,
+    submissions_chall_solves,
+    submissions_team_solves,
+    submissions_only_one_solve;
+
+DROP TABLE
+    teams,
+    categories,
+    challenge_groups,
+    challenges,
+    submissions;
