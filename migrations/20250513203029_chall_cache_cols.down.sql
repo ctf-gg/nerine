09a836e6edc30a6512a51a955731ee73e6263079
voxal <aidenhohoho@gmail.com>
2025-05-14 17:21:31 -0700

    add challenge points index

--- migrations/20250513203029_chall_cache_cols.down.sql
+++ migrations/20250513203029_chall_cache_cols.down.sql
@@ -1,3 +1,5 @@
+DROP INDEX challenge_points;
+
 ALTER TABLE challenges 
 DROP COLUMN c_solves,
 DROP COULMN c_points;
--- migrations/20250513203029_chall_cache_cols.up.sql
+++ migrations/20250513203029_chall_cache_cols.up.sql
@@ -1,3 +1,5 @@
 ALTER TABLE challenges 
 ADD COLUMN c_solves INT NOT NULL DEFAULT 0,
 ADD COLUMN c_points INT NOT NULL DEFAULT 0;
+
+CREATE INDEX challenge_points ON challenges (id, c_points);
