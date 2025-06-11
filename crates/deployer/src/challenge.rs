20f9f19a6e943a2421594d2251260c78e1ef924f
anirudhb <anirudhb@users.noreply.github.com>
2025-06-10 22:51:33 -0700

    add deployer lib and regenerate lockfile

---
+++ crates/deployer/src/challenge.rs
@@ -0,0 +1,163 @@
+use std::{collections::HashMap, fs::File as StdFile, path::PathBuf};
+use eyre::{Context, Result};
+use log::info;
+
+use futures_util::StreamExt;
+use serde::Deserialize;
+use tempdir::TempDir;
+use tokio::fs::{self, File};
+use tokio_util::io::ReaderStream;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Challenge {
+    pub slug: String,
+    pub name: String,
+    pub author: String,
+    pub description: String,
+    pub flag: Flag,
+    pub hidden: Option<bool>,
+    pub provide: Option<Vec<Attachment>>,
+    pub container: Option<Container>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+pub enum Flag {
+    Raw(String),
+    File { file: PathBuf },
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+pub enum Attachment {
+    File(PathBuf),
+    Named {
+        file: PathBuf,
+        r#as: String,
+    },
+    Archive {
+        globs: Vec<String>,
+        // without archive extension
+        #[serde(default = "default_archive_name")]
+        r#as: String,
+        exclude: Option<Vec<PathBuf>>,
+    },
+}
+
+fn default_archive_name() -> String {
+    "chall".to_owned()
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Container {
+    pub build: PathBuf,
+    pub limits: Limits,
+    pub env: Option<HashMap<String, String>>,
+    pub expose: Option<HashMap<u16, ExposeType>>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Limits {
+    pub cpu: Option<u64>,
+    pub mem: Option<u64>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ExposeType {
+    Tcp,
+    Http,
+}
+
+#[derive(Debug, Clone)]
+pub struct DeployableChallenge {
+    chall: Challenge,
+    root: PathBuf,
+}
+
+#[derive(Debug, Clone)]
+/// Expected to be passed by callers.
+pub struct DeployableContext {
+    pub docker: bollard::Docker,
+    pub docker_credentials: Option<bollard::auth::DockerCredentials>,
+    pub image_prefix: String,
+}
+
+impl DeployableChallenge {
+    // FIXME(ani): should this even be async
+    pub async fn from_root(root: PathBuf) -> Result<Self> {
+        let chall_data = fs::read_to_string(root.join("challenge.toml"))
+            // ugh
+            .await
+            .with_context(|| format!("Failed to read challenge.toml in {}", root.display()))?;
+        let chall = toml::from_str::<Challenge>(&chall_data)?;
+        Ok(Self {
+            chall,
+            root,
+        })
+    }
+
+    fn image_id(&self, ctx: &DeployableContext) -> String {
+        format!("{}{}", ctx.image_prefix, self.chall.slug)
+    }
+
+    pub async fn build(&self, ctx: &DeployableContext) -> Result<Option<Vec<bollard::models::BuildInfo>>> {
+        let Some(chall_container) = &self.chall.container else {
+            return Ok(None);
+        };
+
+        let tmp = TempDir::new(&self.chall.slug)?;
+        let context_tar_path = tmp.path().join("docker.tar");
+        {
+            // ugh
+            let tar_file = StdFile::create(&context_tar_path)?;
+            let mut tar_ = tar::Builder::new(tar_file);
+            let context_dir_path = self.root.join(&chall_container.build);
+            tar_.append_dir_all(".", &context_dir_path)
+                .with_context(|| format!("Failed to read Docker context {}", context_dir_path.display()))?;
+            tar_.finish()?;
+        }
+
+        let options = bollard::query_parameters::BuildImageOptionsBuilder::new()
+            // FIXME(ani): idk if it's ideal to tag the image with the repo name in build
+            .t(&self.image_id(ctx))
+            .forcerm(true)
+            .rm(true)
+            .build();
+        let tar_file_r = File::open(&context_tar_path).await?;
+        let tar_file_r = ReaderStream::new(tar_file_r);
+        // TODO: support credentials?
+        let mut build = ctx.docker.build_image(options, None, Some(bollard::body_try_stream(tar_file_r)));
+        let mut build_infos = vec![];
+        while let Some(build_step) = build.next().await {
+            let build_step = build_step.context("Docker build image error")?;
+            if let Some(stream) = &build_step.stream {
+                info!("{}", stream);
+            }
+            build_infos.push(build_step);
+        }
+
+        Ok(Some(build_infos))
+    }
+
+    pub async fn push(&self, ctx: &DeployableContext) -> Result<()> {
+        // TODO: support credentials
+        let mut push = ctx.docker.push_image(
+            &self.image_id(ctx),
+            None::<bollard::query_parameters::PushImageOptions>,
+            ctx.docker_credentials.clone(),
+        );
+
+        while let Some(push_step) = push.next().await {
+            let push_step = push_step.context("Docker image push error")?;
+            info!("{:?}", push_step);
+        }
+
+        Ok(())
+    }
+
+    pub async fn push_attachments(&self) -> Result<()> {
+        /* build and push attachments to s3/gcs */
+        todo!()
+    }
+}
