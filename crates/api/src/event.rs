63ce600be9e04f68ad5fcd25f91e1db1a4137d11
jayden <git@jayden.email>
2025-06-13 18:43:07 +0000

    sekai decay algo

--- crates/api/src/event.rs
+++ crates/api/src/event.rs
@@ -1,7 +1,7 @@
-use std::{cmp::max, fs};
 use axum::{extract::State as StateE, routing::get, Json, Router};
-use serde::{Deserialize, Serialize};
 use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use std::fs;
 
 use crate::{Result, State};
 
@@ -25,12 +25,20 @@ impl Event {
     }
 }
 
-pub fn point_formula(
-    points_min: i32, points_max: i32, solves: i32,
-) -> i32 {
-    return max(points_min, points_max - (points_max - points_min) * solves / 20)
-}
+// ported from https://github.com/blueset/CTFd/blob/luna/CTFd/plugins/dynamic_challenges/decay.py#L72
+pub fn point_formula(points_min: i32, points_max: i32, solves: i32) -> i32 {
+    // note(jayden): ctfd has this configurable per chall, but here im hardcoding to 60 (same as used in sekaictf)
+    let decay = 60.0;
+    let gradient = 10.0;
 
+    let _min = 1.0 + ((gradient - 1.0) / decay);
+    let x = 1.0 + ((gradient - 1.0) / decay) * solves as f64;
+    let ratio = (x / _min).ln() / (gradient / _min).ln();
+    let raw_score = (points_max as f64 - (points_max - points_min) as f64 * ratio).ceil();
+    let value = points_min.max(points_max.min(raw_score as i32));
+
+    value
+}
 /* web routes
    NOTE(ani): keep it small or move into a separate file */
 
