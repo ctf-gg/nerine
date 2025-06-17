95d199dd88d927c070b1f53a51a1f8dea6e15267
anirudhb <anirudhb@users.noreply.github.com>
2025-06-16 20:18:40 -0700

    stop supporting nginx in favor of caddy

--- test/dev.sh
+++ test/dev.sh
@@ -17,14 +17,11 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 tail -f /dev/null | tail -f /dev/null & PID2=$!; PID1=$(jobs -p %+); exec 5>/proc/$PID1/fd/1  6</proc/$PID2/fd/0; disown $PID2; kill $PID1 $PID2
 tail -f /dev/null | tail -f /dev/null & PID2=$!; PID1=$(jobs -p %+); exec 7>/proc/$PID1/fd/1  8</proc/$PID2/fd/0; disown $PID2; kill $PID1 $PID2
 tail -f /dev/null | tail -f /dev/null & PID2=$!; PID1=$(jobs -p %+); exec 9>/proc/$PID1/fd/1  10</proc/$PID2/fd/0; disown $PID2; kill $PID1 $PID2
-tail -f /dev/null | tail -f /dev/null & PID2=$!; PID1=$(jobs -p %+); exec 11>/proc/$PID1/fd/1 12</proc/$PID2/fd/0; disown $PID2; kill $PID1 $PID2
 
 RUST_LOG=debug unbuffer ./target/debug/nerine-api >&5 2>&5 &
 (cd frontend && unbuffer pnpm dev) >&7 2>&7 0</dev/null &
-sudo -n nginx -c $PWD/test/nginx.conf >&9 2>&9 &
-RUST_LOG=debug unbuffer ./target/debug/nerine-deployer-server >&11 2>&11 &
+RUST_LOG=debug unbuffer ./target/debug/nerine-deployer-server >&9 2>&9 &
 while read -r line; do printf "[\033[0;35mnerine-api\033[m] %s\n" "${line}"; done <&6 &
 while read -r line; do printf "[\033[0;34mastro   \033[m] %s\n" "${line}"; done <&8 &
-while read -r line; do printf "[\033[1;30mnginx   \033[m] %s\n" "${line}"; done <&10 &
-while read -r line; do printf "[\033[0;35mnerine-dly\033[m] %s\n" "${line}"; done <&12 &
+while read -r line; do printf "[\033[0;35mnerine-dly\033[m] %s\n" "${line}"; done <&10 &
 wait
